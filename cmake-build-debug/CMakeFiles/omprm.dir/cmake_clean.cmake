cd22544af72c3ffbf07fc3283c14e49ae1b4b996
BMI24 <mitgliedschaftfo@gmx.de>
2020-01-04 13:41:46 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/omprm.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/omprm.dir/main.cpp.o"
+  "libomprm.dll.a"
+  "omprm.exe"
+  "omprm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/omprm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/omprm.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for omprm.
+# This may be replaced when dependencies are built.
