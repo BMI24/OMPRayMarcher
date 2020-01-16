24236c0d0dba2558b2c934cbea1022536034b5cf
BMI24 <mitgliedschaftfo@gmx.de>
2020-01-16 18:18:15 +0100

    Replace math methods returning double by counterpart methods returning floats

--- ray_marcher.cpp
+++ ray_marcher.cpp
@@ -1,4 +1,3 @@
-#include <sys/param.h>
 #include <cmath>
 #include <vector>
 #include <cfloat>
@@ -123,11 +122,11 @@ mat4x4 generate_mat_rot_z(float angle)
     return rot_mat;
 }
 
-inline void write_r(uint8_t* image, int image_x, const int image_y, const int x_size, const uint8_t value);
-inline void write_g(uint8_t* image, int image_x, const int image_y, const int x_size, const uint8_t value);
-inline void write_b(uint8_t* image, int image_x, const int image_y, const int x_size, const uint8_t value);
-inline void render_pixel(uint8_t* image, int image_x, const int image_y, const int x_size,
-        const float x_angle, const float y_angle, const float camera_x, const float camera_y, const float camera_z,
+inline void write_r(uint8_t* image, int image_x, int image_y, int x_size, uint8_t value);
+inline void write_g(uint8_t* image, int image_x, int image_y, int x_size, uint8_t value);
+inline void write_b(uint8_t* image, int image_x, int image_y, int x_size, uint8_t value);
+inline void render_pixel(uint8_t* image, int image_x, int image_y, int x_size,
+        float x_angle, float y_angle, float camera_x, float camera_y, float camera_z,
         float far_clip, object_interface** objects, int objects_length, light** lights, int lights_length);
 inline float deg2rad (float degrees);
 inline float distance_between(float x1, float y1, float z1, float x2, float y2, float z2);
@@ -157,8 +156,8 @@ void render(const int image_x_size, const int image_y_size, uint8_t* image, obje
 
     for (int y = 0; y < image_y_size; ++y) {
         for (int x = 0; x < image_x_size; ++x) {
-            float x_angle = camera_x_view_angle - (image_x_fov / 2) + (image_x_fov / image_x_size) * x;
-            float y_angle = camera_y_view_angle - (image_y_fov / 2) + (image_y_fov / image_y_size) * y;
+            float x_angle = camera_x_view_angle - (image_x_fov / 2) + (image_x_fov / (float)image_x_size) * (float)x;
+            float y_angle = camera_y_view_angle - (image_y_fov / 2) + (image_y_fov / (float)image_y_size) * (float)y;
             render_pixel(image, x, y, image_x_size, x_angle, y_angle, camera_x_pos, camera_y_pos, camera_z_pos,
                     far_clip, objects, objects_length, lights, lights_length);
         }
@@ -240,13 +239,13 @@ inline void write_b(uint8_t* image, const int image_x, const int image_y, const 
 
 inline float distance_between(float x1, float y1, float z1, float x2, float y2, float z2) {
     return sqrtf(
-            pow(x1 - x2, 2)
-            + pow(y1 - y2, 2)
-            + pow(z1 - z2, 2));
+            powf(x1 - x2, 2)
+            + powf(y1 - y2, 2)
+            + powf(z1 - z2, 2));
 }
 
 inline float deg2rad(float degrees) {
-    static const double pi_on_180 = 4.0 * atanf(1.0) / 180.0;
+    static const float pi_on_180 = 4.f * atanf(1.0) / 180.f;
     return degrees * pi_on_180;
 }
 
@@ -315,7 +314,7 @@ vec3 phong_contrib_for_light(vec3 diffuse_color, vec3 specular_color, float alph
         return light_intensity * (diffuse_color * dot_o_n);
     }
 
-    return light_intensity * (diffuse_color * dot_o_n + specular_color * pow(dot_r_c, alpha));
+    return light_intensity * (diffuse_color * dot_o_n + specular_color * powf(dot_r_c, alpha));
 }
 
 float scene_sdf_ignore(object_interface *const *objects, int objects_length, vec3 position, int ignore_object_index)
